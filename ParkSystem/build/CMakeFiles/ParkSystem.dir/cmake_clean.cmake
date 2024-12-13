ce25763ef9d6b096387d9d78b264801cc84f35ac
AlkaidYoukou <abckuil678@gmail.com>
2024-12-13 09:26:00 +0800

    cmake生成makefile

---
+++ ParkSystem/build/CMakeFiles/ParkSystem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParkSystem.dir/src/main.c.o"
+  "CMakeFiles/ParkSystem.dir/src/main.c.o.d"
+  "ParkSystem"
+  "ParkSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ParkSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
